32d5eec79838d886423ec21e041596912fc72c54
Sean Sundberg <seansund@us.ibm.com>
2020-05-15 09:37:32 -0500

    Waits for deployment to start before checking the rollout status

--- scripts/deploy-instance.sh
+++ scripts/deploy-instance.sh
@@ -46,7 +46,22 @@ kubectl apply -f ${YAML_FILE} -n "${NAMESPACE}"
 
 sleep 2
 
-kubectl rollout status deployment/${NAME} -n "${NAMESPACE}" || exit 1
+DEPLOYMENT="${NAME}"
+
+count=0
+until kubectl get deployment/${DEPLOYMENT} 1> /dev/null 2> /dev/null; do
+  if [[ ${count} -eq 12 ]]; then
+    echo "Timed out waiting for deployment/${DEPLOYMENT} to start"
+    exit 1
+  else
+    count=$((count + 1))
+  fi
+
+  echo "Waiting for deployment/${DEPLOYMENT} to start"
+  sleep 10
+done
+
+kubectl rollout status deployment/${DEPLOYMENT} -n "${NAMESPACE}" || exit 1
 
 if [[ -n "${OUTPUT_FILE}" ]]; then
   POD_NAME=$(kubectl get pods -n "${NAMESPACE}" -l app=${NAME} -o jsonpath='{range .items[]}{.metadata.name}{"\n"}{end}')
