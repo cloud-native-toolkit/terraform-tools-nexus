eac3da44f7452453826d9ebc8e93153c2142de4e
Sean Sundberg <seansund@us.ibm.com>
2020-06-03 00:20:06 -0500

    Adds OCP console application menu
    - Adds applicationMenu and ingressSubdomain values to tool-config to create application menu on OCP
    - Sets name in tool-config to Nexus to display properly
    - Adds resource to delete the console link before creating a new one
    
    ibm-garage-cloud/planning#392

--- main.tf
+++ main.tf
@@ -41,8 +41,20 @@ data "local_file" "nexus-password" {
   filename = local.password_file
 }
 
+resource "null_resource" "delete-consolelink" {
+  count = var.cluster_type != "kubernetes" ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "kubectl delete consolelink -l grouping=garage-cloud-native-toolkit -l app=nexus || exit 0"
+
+    environment = {
+      KUBECONFIG = var.cluster_config_file
+    }
+  }
+}
+
 resource "helm_release" "nexus-config" {
-  depends_on = [null_resource.nexus-instance]
+  depends_on = [null_resource.nexus-instance, null_resource.delete-consolelink]
 
   name         = "nexus"
   repository   = "https://ibm-garage-cloud.github.io/toolkit-charts/"
@@ -63,5 +75,20 @@ resource "helm_release" "nexus-config" {
   set {
     name  = "password"
     value = local.password
+  }
+
+  set {
+    name  = "applicationMenu"
+    value = var.cluster_type != "kubernetes"
+  }
+
+  set {
+    name  = "ingressSubdomain"
+    value = var.ingress_subdomain
+  }
+
+  set {
+    name  = "name"
+    value = "Nexus"
   }
 }
